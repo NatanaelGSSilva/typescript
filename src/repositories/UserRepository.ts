59cf1f0ab3304a7f949be17c88bf7a2554d91df3
luiz felipe picoli <felipedelibra@hotmail.com>
2020-05-15 21:17:07 -0300

    atualização do projeto

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -18,6 +18,16 @@ import User from '../models/User';
   public findByEmail(email: string): User | undefined{
     return this.users.find(value => value.email === email);
   }
+  public delete(id: string): Array<user> {
+    const index = this.users.findIndex(obj => obj.id === id);
+
+    if (index === -1) {
+      throw Error(`Não foi encontrado um ticket com o id ${id}`);
+    }
+
+    this.user.splice(index, 1);
+    return this.user;
+  }
 
   public save({// vai receber o que do produto, eu peguei esses carinhas do meu produto
     nome,
